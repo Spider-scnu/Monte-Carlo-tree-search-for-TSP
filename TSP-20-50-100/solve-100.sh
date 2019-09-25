bd7420d944a83412c7a38aa0a412ac32b83f1540
TSP <44106649+Spider-scnu@users.noreply.github.com>
2019-09-25 18:54:16 +0800

    Update solve-100.sh

--- TSP-20-50-100/solve-100.sh
+++ TSP-20-50-100/solve-100.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 # author: 
-
-make 
+make
 STARTTIME=$(date +%s)
 tsp=("./instances/tsp100_test_concorde.txt")
 instancenum=(100)
-
 j=0
-sources100=("./results/100-batch/result_1.txt" "./results/100-batch/result_2.txt" "./results/100-batch/result_3.txt" "./results/100-batch/result_4.txt" "./results/100-batch/result_5.txt" "./results/100-batch/result_6.txt" "./results/100-batch/result_7.txt" "./results/100-batch/result_8.txt" "./results/100-batch/result_9.txt" "./results/100-batch/result_10.txt" "./results/100-batch/result_11.txt" "./results/100-batch/result_12.txt" "./results/100-batch/result_13.txt" "./results/100-batch/result_14.txt" "./results/100-batch/result_15.txt" "./results/100-batch/result_16.txt" "./results/100-batch/result_17.txt" "./results/100-batch/result_18.txt" "./results/100-batch/result_19.txt" "./results/100-batch/result_20.txt" "./results/100-batch/result_21.txt" "./results/100-batch/result_22.txt" "./results/100-batch/result_23.txt" "./results/100-batch/result_24.txt" "./results/100-batch/result_25.txt" "./results/100-batch/result_26.txt" "./results/100-batch/result_27.txt" "./results/100-batch/result_28.txt" "./results/100-batch/result_29.txt" "./results/100-batch/result_30.txt" "./results/100-batch/result_31.txt" "./results/100-batch/result_32.txt")
+sources100=("./results/100/result_1.txt" "./results/100/result_2.txt" "./results/100/result_3.txt" "./results/100/result_4.txt" "./results/100/result_5.txt" "./results/100/result_6.txt" "./results/100/result_7.txt" "./results/100/result_8.txt" "./results/100/result_9.txt" "./results/100/result_10.txt" "./results/100/result_11.txt" "./results/100/result_12.txt" "./results/100/result_13.txt" "./results/100/result_14.txt" "./results/100/result_15.txt" "./results/100/result_16.txt" "./results/100/result_17.txt" "./results/100/result_18.txt" "./results/100/result_19.txt" "./results/100/result_20.txt" "./results/100/result_21.txt" "./results/100/result_22.txt" "./results/100/result_23.txt" "./results/100/result_24.txt" "./results/100/result_25.txt" "./results/100/result_26.txt" "./results/100/result_27.txt" "./results/100/result_28.txt" "./results/100/result_29.txt" "./results/100/result_30.txt" "./results/100/result_31.txt" "./results/100/result_32.txt")
 threads=$1
 for ((i=0;i<$threads;i++));do
 {
